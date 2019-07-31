114a8c001c8444bb58c118051089b57d6baba3ac
GabrielFagundes001 <37418298+GabrielFagundes001@users.noreply.github.com>
2019-07-31 09:28:53 -0300

    Add files via upload

---
+++ enem.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Abrindo portas</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!--   css do site -->
+
+<link rel="stylesheet" type="text/css" href="tcc.css">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/one-page-wonder.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Abrindo portas</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="logado.html">Revisar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="informacao.html">Notícias</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="cadastro/perfil.php">Perfil</a>
+          </li>
+            <li class="nav-item">
+            <a class="nav-link" href="sobre.html">Sobre</a>
+          </li>
+           <li class="nav-item">
+            <a class="nav-link" href="index.php"><font color="red"> Sair</font></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  
---
+++ index.php
@@ -0,0 +1,163 @@
+
+
+<!DOCTYPE html>
+<html lang="pt br">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Abrindo portas</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/one-page-wonder.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">Abrindo portas</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="sistema_login/login-php/entrar.php">Login</a>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link" href="cadastro/cadastro.php">Cadastro</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<div class="bd-example">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/inicio.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+         <font color="white"> <h1></h1></font>
+        <h1 class="masthead-heading mb-0">Teste seu conhecimento. </h1>
+        <h2 class="masthead-subheading mb-0">Faça simulados para o ENEM</h2>
+        <a href="cadastro/cadastro.php" class="btn btn-primary btn-xl rounded-pill mt-5">Cadastrar-se</a>
+        </div>
+      </div>
+  </div>
+</div>
+
+
+  
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="img/01.png" alt="">
+          </div>
+        </div>
+
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">Breve revisão para o ENEM.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="img/02.png" alt="">
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="p-5">
+            <h2 class="display-4">Revisão para programadores.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<br><br><br>
+
+<footer class="py- bg-secondary">
+		<div class="container">
+<nav class="">
+<div class="container">
+<div class="card-deck py-5 bg-muted">
+  <div class="card">
+    <img src="img/card01.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Notícias</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="img/card02.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Dicas</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="img/card03.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Se organize</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+</div>
+</nav>
+</div>
+</footer>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-black">
+    <div class="container">
+      <p class="m-0 text-center text-white small">Abrindo portas &copy; Criado em 2019</p>
+    </div>
+
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+<!-- https://colorlib.com/preview/#kanox
+ -->
+</html>
